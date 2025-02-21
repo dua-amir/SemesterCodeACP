09db487b030a5c7bc3efd7a0bc0006fb2b318d62
dua-amir <duaamir4211@gmail.com>
2025-02-21 17:34:38 +0500

    ACP practice

---
+++ 21Feb-practice-1.js
@@ -0,0 +1,19 @@
+function createArray() {
+    const arr = [
+        {
+            sapid: 47849,
+            name: 'dua'
+        },
+        {
+            sapid: 46484,
+            name: 'zainab'
+        },
+        {
+            sapid: 46462,
+            name: 'samreen'
+        }
+    ];
+    return arr;
+}
+module.exports = createArray;
+console.log(createArray)
